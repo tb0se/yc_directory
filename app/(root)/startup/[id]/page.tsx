96be5f3e534369561fbb6d9ae93c0bb76db3746c
tb0se <rachiditbose@gmail.com>
2025-06-30 12:29:46 +0200

    add parallel fetching & playlists

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -6,9 +6,10 @@ import { notFound } from 'next/navigation';
 
 import { formatDate } from '@/lib/utils';
 import { client } from '@/sanity/lib/client';
-import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
+import { PLAYLIST_BY_SLUG_QUERY, STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
 import { Skeleton } from '@/components/ui/skeleton';
 import View from '@/components/View';
+import StartupCard, { StartupCardType } from '@/components/StartupCard';
 
 const md = markdownit();
 
@@ -21,7 +22,8 @@ type PageProps = Readonly<{
 export default async function Page({ params }: PageProps) {
     const id = (await params).id;
 
-    const post = await client.fetch(STARTUP_BY_ID_QUERY, { id })
+    const [post, { select: editorPosts }] = await Promise.all([client.fetch(STARTUP_BY_ID_QUERY, { id }), client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: 'editor-picks' })]);
+
     if (!post) return notFound();
 
     const parsedContent = md.render(post?.pitch || '');
@@ -59,7 +61,16 @@ export default async function Page({ params }: PageProps) {
                     }
                 </div>
                 <hr className='divider' />
-                {/* EDITOR SELECTED STARTUPS */}
+                {
+                    editorPosts?.length > 0 && (<div className='max-w-4xl mx-auto'>
+                        <p className='text-30-semibold'>Editor Picks</p>
+                        <ul className='mt-7 card_grid-sm'>
+                            {
+                                editorPosts.map((post: StartupCardType, index: number) => (<StartupCard key={index} post={post} />))
+                            }
+                        </ul>
+                    </div>)
+                }
                 <Suspense fallback={<Skeleton className='view_skeleton' />}>
                     <View id={id} />
                 </Suspense>
